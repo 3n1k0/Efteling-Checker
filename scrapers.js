2eeb8f51caf9a1689fd84726348c29dab6897bda
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2020-05-14 16:25:22 +0200

    Create scrapers.js

---
+++ scrapers.js
@@ -0,0 +1,24 @@
+const rp = require("request-promise");
+const url = "https://www.efteling.com/en/park/tickets";
+var player = require("play-sound")((opts = {}));
+
+setInterval(() => {
+  rp(url).then(function (html) {
+    if (
+      html.includes(
+        "Currently we are working on a reservation system to reserve your visit to Efteling. Keep an eye on the website for more information."
+      )
+    ) {
+      console.log("Meg mindig benne van.");
+    } else {
+      console.log("nincsbenne");
+      rp.post("http://api.justyo.co/yo/", {
+        form: {
+          api_token: "01bd8b34-faf7-463e-8948-aae13f01ae5d",
+          username: "ANNIEKOSTOLANY",
+          link: "http://google.com",
+        },
+      });
+    }
+  });
+}, 300000);
